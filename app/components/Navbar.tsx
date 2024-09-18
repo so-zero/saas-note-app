baa16f07674cdfff6c020304bb1a6aed2169ce92
so-zero <zero_s0@naver.com>
2024-09-18 17:04:29 +0900

    auth

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,15 @@
 import Link from "next/link";
 import { ModeToggle } from "./darkmode/ModeToggle";
 import { Button } from "@/components/ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const { isAuthenticated } = getKindeServerSession();
   return (
     <nav className="border-b bg-background h-[10vh] flex items-center px-8">
       <div className="container mx-auto flex items-center justify-between">
@@ -10,10 +17,20 @@ export default function Navbar() {
           <h1 className="font-bold text-2xl">SaaS Note</h1>
         </Link>
         <div className="flex items-center gap-x-5">
-          <div className="flex items-center gap-x-5">
-            <Button>로그인</Button>
-            <Button variant="secondary">회원가입</Button>
-          </div>
+          {(await isAuthenticated()) ? (
+            <LogoutLink>
+              <Button>로그아웃</Button>
+            </LogoutLink>
+          ) : (
+            <div className="flex items-center gap-x-5">
+              <LoginLink>
+                <Button>로그인</Button>
+              </LoginLink>
+              <RegisterLink>
+                <Button variant="secondary">회원가입</Button>
+              </RegisterLink>
+            </div>
+          )}
           <ModeToggle />
         </div>
       </div>
